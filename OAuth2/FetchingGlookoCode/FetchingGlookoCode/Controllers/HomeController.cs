30fd0d013d2c452237f2635dbfc00a87979f6a86
Kristijan Novoselić <kristijan@codecons.com>
2014-09-11 17:57:46 +0200

    Implemented basic OAuth2 authorization and authentication

--- OAuth2/FetchingGlookoCode/FetchingGlookoCode/Controllers/HomeController.cs
+++ OAuth2/FetchingGlookoCode/FetchingGlookoCode/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace FetchingGlookoCode.Controllers
@@ -10,7 +12,38 @@ namespace FetchingGlookoCode.Controllers
     {
         public ActionResult Index()
         {
+            Dictionary<String, String> queryParams = new Dictionary<String, String>();
+            queryParams["client_id"] = WebConfigurationManager.AppSettings["GlookoApplicationID"];
+            queryParams["redirect_uri"] = Url.Action("Callback", "OAuth", null, Request.Url.Scheme);
+            queryParams["response_type"] = "code";
+            queryParams["scope"] = "glooko_code";
+            UriBuilder glookoAuthorizationURI = new UriBuilder("https", "staging.glooko.com", 443, "/oauth/authorize", BuildQueryString(queryParams));
+
+            ViewBag.GlookoAuthorizationURI = glookoAuthorizationURI.ToString();
             return View();
+        }
+
+        private String BuildQueryString(Dictionary<String, String> queryParams)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < queryParams.Count; i++)
+            {
+                var param = queryParams.ElementAt(i);
+                if (i == 0)
+                {
+                    result.Append("?");
+                }
+                else
+                {
+                    result.Append("&");
+                }
+                result.Append(param.Key);
+                result.Append("=");
+                result.Append(HttpUtility.UrlEncode(param.Value));
+
+            }
+            return result.ToString();
         }
     }
 }
---
+++ OAuth2/FetchingGlookoCode/FetchingGlookoCode/Controllers/OAuthController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace FetchingGlookoCode.Controllers
+{
+    public class OAuthController : Controller
+    {
+        public ActionResult Callback([Bind(Prefix = "code")]String authorization_code = null, String error = null, [Bind(Prefix = "error_description")]String errorDescription = null)
+        {
+            // check if authorization code is received. If not, an error has occurred.
+            if (String.IsNullOrWhiteSpace(authorization_code))
+            {
+                ViewBag.Error = error;
+                ViewBag.ErrorDescription = errorDescription;
+                return View("CallbackError");
+            }
+
+            // exchange authorization code for authentication token, refresh token and glooko code
+            using (WebClient webClient = new WebClient())
+            {
+                NameValueCollection postData = new NameValueCollection()
+                {
+                    {"client_id", WebConfigurationManager.AppSettings["GlookoApplicationID"] },
+                    {"client_secret", WebConfigurationManager.AppSettings["GlookoApplicationSecret"]},
+                    {"redirect_uri", Url.Action("Callback", "OAuth", null, Request.Url.Scheme)},
+                    {"grant_type", "authorization_code"},
+                    {"code", authorization_code}
+                };
+                try
+                {
+                    var response = webClient.UploadValues("https://staging.glooko.com/oauth/token", postData);
+                    ViewBag.Response = PrettifyIfJSON(System.Text.Encoding.UTF8.GetString(response));
+                }
+                catch (WebException e)
+                {
+                    ViewBag.Error = e.ToString();
+                    using (StreamReader reader = new StreamReader(e.Response.GetResponseStream()))
+                    {
+                        ViewBag.Response = PrettifyIfJSON(reader.ReadToEnd());
+                    }
+                }
+            }
+            return View();
+        }
+
+        private String PrettifyIfJSON(String toPrettify)
+        {
+            try
+            {
+                return Newtonsoft.Json.Linq.JObject.Parse(toPrettify).ToString(Newtonsoft.Json.Formatting.Indented);
+            }
+            catch (Newtonsoft.Json.JsonException)
+            {
+                return toPrettify;
+            }
+        }
+    }
+}
